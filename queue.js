4aff3143f8d7fb68ba6b209fd4ffa3b914913373
Neeti <neeti.i.bhatt@gmail.com>
2023-11-17 16:01:05 -0800

    updated queue.js code

--- queue.js
+++ queue.js
@@ -20,6 +20,21 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    //create a new node
+    let newNode = new Node(val);
+
+    //if the queue is empty then the new node would be first and last node
+    if (!this.first) {
+      this.first = newNodeode;
+      this.last = newNodeode;
+    } else {
+      //if the queue is not empty then add the node at the end of the queue
+      this.last.next = newNodeode;
+      this.last = newNodeode;
+    }
+
+    //increase the size of the queue
+    this.size++;
 
   }
 
@@ -28,17 +43,35 @@ class Queue {
 
   dequeue() {
 
+    //if the queue is empty then we can't delete the node from it
+    if (!this.first) throw new Error("Can't dequeue from an empty queue.");
+
+    //if there is only one node 
+    let temp = this.first;
+    if (this.first == this.last) {
+      this.last = null;
+    }
+    //if the there are multiple nodes then remove the first node
+    this.first = this.first.next;
+    //decrease the queue size
+    this.size--;
+    return temp.val;
+
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    //return the first node (front node)
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
+    
+    //return true if the queue size is 0
+    return this.size === 0;
 
   }
 }
